96d00092c07e9413411037d4e3df998d97fe82e0
Thea Tiglao <thea.tiglao@strideconsulting.org>
2023-11-28 17:09:04 +0800

    D

--- Thea/cavitex/google_popular_times/api/api.R
+++
@@ -1,9 +0,0 @@
-library(httr)
-library(jsonlite)
-
-get_location <- function() {
-  res <- POST('http://localhost:8000/location')
-  data <- fromJSON(rawToChar(res$content))
-  
-  return(Vectorize(data))
-}
