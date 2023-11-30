1304d0908aefcaa49cb62a23722a7376731f2e28
thea-stride <thea.tiglao@strideconsulting.org>
2023-11-30 11:28:01 +0800

    Waze RShiny per hour

--- Thea/cavitex/main.R
+++ Thea/cavitex/main.R
@@ -3,48 +3,289 @@ library(jsonlite)
 library(tidyverse)
 library(reticulate)
 library(later)
+library(lares)
+library(shinyTime)
+library(lubridate)
 
-# source('D:\\Codes\\cavitex\\google_popular_times\\main.R')
-source('D:\\Codes\\cavitex\\waze\\main.R')
+# for google popular times
+source('D:\\Codes\\cavitex\\google_popular_times\\get_week_of_day.R')
+source('D:\\Codes\\cavitex\\google_popular_times\\google_popular_times.R')
+source('D:\\Codes\\cavitex\\google_popular_times\\api\\api.R')
 
-# source_python('D:\\Codes\\cavitex\\waze\\waze_feeds\\download_waze.py')
+source_python('D:\\Codes\\cavitex\\google_popular_times\\api\\api.py')
+
+# for waze
+source('D:\\Codes\\cavitex\\waze\\draw_waze.R')
+date_now = format(Sys.time(), '%Y-%m-%d')
+folder_path <- 'D:\\Codes\\cavitex\\waze\\downloaded_waze'
 
 ui <- fillPage(
   tags$head(
     tags$style(
       HTML(
-        'body { height: 100%; background-color: white; }'
+        'body { height: 100%; background-color: white; }',
+        '#id_search { float: right; background-color: #46923C; color: white; border: 2px solid #46923C; }'
       )
     )
   ),
   navbarPage(
+    # position = c('fixed-top'),
     title = 'CAVITEX',
-    tabPanel(
-      title = "Google Popular Times",
-      uiOutput(
-        outputId = 'id_gpt',
-        height = 1000
-      )
-    ),
     tabPanel(
       title = "Waze Data",
       uiOutput(
         outputId = 'id_waze'
       )
+    ),
+    tabPanel(
+      title = "Google Popular Times",
+      uiOutput(
+        outputId = 'id_gpt'
+      )
     )
   )
 )
 
-server <- function(input, output, session) {
-  # r_to_py(download_waze())
+default <- function(input, output) {
+  file_path <- paste(folder_path, date_now, strftime(Sys.time(), '%H%M'), sep = '\\')
+  file <- paste(file_path, '.json', sep = '')
   
-  output$id_gpt <- renderUI({
-    gpt
-  })
+  if (file.exists(file)) {
+    map <- draw_poly_map(file, 'All')
+    
+    output$id_map_waze <- renderUI({
+      div(
+        map,
+        style = 'height: 90vh; overflow-y: auto;'
+      )
+    })
+  } else {
+    # no such file
+    showModal(
+      modalDialog(
+        title = 'No file found',
+        'Please choose other date or time.',
+        easyClose = TRUE,
+        footer = div(
+          modalButton('Close')
+        )
+      )
+    )
+  }
+}
 
+server <- function(input, output, session) {
+  # ========== WAZE FEEDS ========== #
   output$id_waze <- renderUI({
-    waze
+    sidebarLayout(
+      sidebarPanel(
+        style = 'height: 50vh;',
+        width = 3,
+        dateInput(
+          inputId = 'id_date',
+          label = 'Choose date:',
+          min = '2023-01-01',
+          max = '2024-12-31',
+          value = date_now
+        ),
+        timeInput(
+          inputId = 'id_time',
+          label = 'Choose time (hour:minute):',
+          value = Sys.time(),
+          seconds = FALSE
+        ),
+        selectInput(
+          inputId = 'id_type',
+          label = 'Choose type:',
+          choices = c('All', 'Jam', 'Road Closed', 'Hazard', 'Accident'),
+          selected = 'All',
+          width = '100%'
+        ),
+        actionButton(
+          class = 'id_search',
+          inputId = 'id_search',
+          label = 'Search'
+        )
+      ),
+      mainPanel(
+        style = 'height: 100%;',
+        width = 9,
+        uiOutput(
+          outputId = 'id_map_waze'
+        )
+      )
+    )
   })
+  
+  default(input, output)
+  
+  observeEvent(
+    input$id_search, {
+      input_date <- input$id_date
+      input_time <- strftime(input$id_time, '%H%M')
+      
+      file_path <- paste(folder_path, input_date, input_time, sep = '\\')
+      file <- paste(file_path, '.json', sep = '')
+      
+      if (file.exists(file)) {
+        map <- draw_poly_map(file, input$id_type)
+        
+        output$id_map_waze <- renderUI({
+          div(
+            map,
+            style = 'height: 90vh; overflow-y: auto;'
+          )
+        })
+      } else {
+        # no such file
+        showModal(
+          modalDialog(
+            title = 'No file found',
+            'Please choose other date or time.',
+            easyClose = TRUE,
+            footer = div(
+              modalButton('Close')
+            )
+          )
+        )
+      }
+    }
+  )
+  
+  # ========== GOOGLE POPULAR TIMES ========== #
+  output$id_gpt <- renderUI({
+    sidebarLayout(
+      sidebarPanel(
+        style = 'height: 90vh;',
+        width = 4,
+        # search
+        div(
+          style = 'padding: 10px; margin-bottom: 30px',
+          h3('Google Popular Times', style = 'font-weight: bold; color: #46923C;'), # title
+          uiOutput(outputId = 'id_location')
+        ),
+        # week picker
+        div(
+          style = 'padding: 10px;',
+          uiOutput(outputId = 'id_week'),
+        ),
+        # graph
+        imageOutput(outputId = 'id_graph')
+      ),
+      mainPanel(
+        style = 'height: 90vh;',
+        width = 8,
+        leafletOutput(outputId = 'id_map_gpt', width = '100%', height = '100%')
+      )
+    )
+  })
+  
+  location <- get_location()
+  
+  if (location$status == 200) {
+    output$id_location <- renderUI({
+      selectInput(
+        inputId = 'id_toll_plaza',
+        width = '100%',
+        label = 'Location',
+        choices = location$location$name,
+        multiple = FALSE
+      )
+    })
+  } else {
+    # empty place name
+    showModal(
+      modalDialog(
+        title = 'Server connection',
+        'No fetched location.',
+        easyClose = TRUE,
+        footer = div(
+          modalButton('Close')
+        )
+      )
+    )
+  }
+  
+  # search place; show map
+  observeEvent(
+    input$id_toll_plaza, {
+      # get location info
+      json_address <- r_to_py(get_address(input$id_toll_plaza))
+      address <<- json2vector(json_address)
+      
+      # get day of week
+      day_week <<- weekdays(Sys.Date())
+      day_number <- get_week_of_day(day_week)
+      
+      # get place information
+      df_place <- popular_times_graph(
+        input$id_toll_plaza,
+        address$coordinates.address,
+        day_week,
+        day_number
+      )
+      
+      # render map
+      output$id_map_gpt <- renderLeaflet({
+        leaflet(height = '100%') %>%
+          addTiles() %>%
+          addMarkers(
+            lng = as.double(address$coordinates.lng),
+            lat = as.double(address$coordinates.lat),
+            popup = paste(paste('<b>Name:</b>', input$id_toll_plaza, sep = ' '),
+                          paste('<b>Address:</b>', address$coordinates.address, sep = ' '),
+                          sep = '\n')
+          )
+      })
+      
+      # show week picker
+      output$id_week <- renderUI({
+        selectInput(
+          inputId = 'id_week_of_day',
+          width = '100%',
+          label = 'Week of the day',
+          choices = list('Monday', 'Tuesday', 'Wednesday',
+                         'Thursday', 'Friday', 'Saturday', 'Sunday'),
+          selected = day_week,
+          multiple = FALSE
+        )
+      })
+      
+      # show bar graph
+      output$id_graph <- renderImage(
+        list(src = df_place$file, width = '100%', height = '300px'),
+        deleteFile = FALSE
+      )
+    }
+  )
+  
+  # week of the day picker
+  observeEvent(
+    input$id_week_of_day, {
+      # get day of week
+      if (input$id_week_of_day != day_week) {
+        day_number <- get_week_of_day(input$id_week_of_day)
+        
+        # get place information
+        df_place <- popular_times_graph(
+          input$id_toll_plaza,
+          address$coordinates.address,
+          input$id_week_of_day,
+          day_number
+        )
+        
+        # show bar graph
+        output$id_graph <- renderImage(
+          list(src = df_place$file, width = '100%', height = '300px'),
+          deleteFile = FALSE
+        )
+      } else {
+        # clear day_week
+        day_week <<- ''
+      }
+    }
+  )
+  
 }
 
 shinyApp(ui = ui, server = server)
--- Thea/cavitex/waze/draw_waze.R
+++ Thea/cavitex/waze/draw_waze.R
@@ -3,71 +3,156 @@ library(tidyverse)
 library(leaflet)
 
 draw_poly_map <- function(file_name, type) {
-  type_ <- gsub(" ", "_", toupper(type))
-  print(type_)
+  type_ <- gsub(' ', '_', toupper(type))
   
   waze_data <- fromJSON(file_name)
-  # print(waze_data)
   
-  # extract alerts data
-  dfalerts <- as.data.frame(waze_data$alerts)
-  
-  # check alert type and set marker color
-  map_dfalerts <- dfalerts %>%
-    mutate(color = case_when(str_detect(type, "JAM") ~ "green",
-                             str_detect(type, "ROAD_CLOSED") ~ "blue",
-                             str_detect(type, "HAZARD") ~ "orange",
-                             str_detect(type, "ACCIDENT") ~ "red",
-                             TRUE ~ "a default")
-    )
-  
-  # extract jams data
-  dfjams <- as.data.frame(waze_data$jams)
-  dfjams_length <- length(waze_data$jams$line) # get jam line count
-  
-  # create leaflet map
-  # add markers (alert); legend
-  map_combined <- leaflet(
-    height = 775
-  ) %>%
-    addTiles() %>%
-    addLegend( # for circle markers
-      "topright",
-      title = "Map Marker Legend",
-      colors = c("green", "blue", "orange", "red"),
-      label = c("Jam", "Road Closed", "Hazard", "Accident"),
-      opacity = 1
+  if (type_ == 'ALL') { # ========== ALL DATA
+    # extract alerts data
+    dfalerts <- as.data.frame(waze_data$alerts)
+    
+    # check alert type and set marker color
+    map_dfcolor <- dfalerts %>%
+      mutate(color = case_when(str_detect(type, "JAM") ~ "green",
+                               str_detect(type, "ROAD_CLOSED") ~ "blue",
+                               str_detect(type, "HAZARD") ~ "orange",
+                               str_detect(type, "ACCIDENT") ~ "red",
+                               TRUE ~ "a default")
+      )
+    
+    # extract jams data
+    dfjams <- as.data.frame(waze_data$jams)
+    dfjams_length <- length(waze_data$jams$line) # get jam line count
+    
+    # create leaflet map
+    # add markers (alert); legend
+    map_combined <<- leaflet(
+      height = 695
     ) %>%
-    addLegend( # for polylines
-      "bottomright",
-      title = "Map Polyine Legend",
-      colors = c("green"),
-      label = c("Jam"),
-      opacity = 1
+      addTiles() %>%
+      addLegend( # for circle markers
+        "topright",
+        title = "Map Marker Legend",
+        colors = c("green", "blue", "orange", "red"),
+        label = c("Jam", "Road Closed", "Hazard", "Accident"),
+        opacity = 1
+      ) %>%
+      addLegend( # for polylines
+        "bottomright",
+        title = "Map Polyine Legend",
+        colors = c("green"),
+        label = c("Jam"),
+        opacity = 1
+      ) %>%
+      addCircleMarkers(
+        data = map_dfcolor,
+        lng = ~location$x,
+        lat = ~location$y,
+        popup = ~subtype,
+        color = ~color,
+        opacity = 1,
+        fillOpacity = 1,
+        weight = 2,
+        radius = 4
+      )
+    
+    # add polyline (jam/traffic)
+    for (i in 1:dfjams_length) {
+      map_combined <<- addPolylines(
+        map = map_combined,
+        data = dfjams,
+        lng = ~line[[i]]$x,
+        lat = ~line[[i]]$y,
+        weight = 2,
+        opacity = 1,
+        color = "green"
+      )
+    }
+    
+  } else if (type_ == 'JAM') { # ========== JAM ONLY
+    # extract jams data
+    dfjams <- as.data.frame(waze_data$jams)
+    dfjams_length <- length(waze_data$jams$line) # get jam line count
+    
+    # create leaflet map
+    # add markers (alert); legend
+    map_combined <<- leaflet(
+      height = 775
     ) %>%
-    addCircleMarkers(
-      data = map_dfalerts,
-      lng = ~location.x,
-      lat = ~location.y,
-      popup = ~subtype,
-      color = ~color,
-      opacity = 1,
-      fillOpacity = 1,
-      weight = 2,
-      radius = 4
-    )
-  
-  # add polyline (jam/traffic)
-  for (i in 1:dfjams_length) {
-    map_combined <- addPolylines(
-      map = map_combined,
-      data = dfjams,
-      lng = ~line[[i]]$x,
-      lat = ~line[[i]]$y,
-      weight = 2,
-      opacity = 1,
-      color = "green"
-    )
+      addTiles() %>%
+      addLegend( # for circle markers
+        "topright",
+        title = "Map Marker Legend",
+        colors = c("green", "blue", "orange", "red"),
+        label = c("Jam", "Road Closed", "Hazard", "Accident"),
+        opacity = 1
+      ) %>%
+      addLegend( # for polylines
+        "bottomright",
+        title = "Map Polyine Legend",
+        colors = c("green"),
+        label = c("Jam"),
+        opacity = 1
+      )
+    
+    # add polyline (jam/traffic)
+    for (i in 1:dfjams_length) {
+      map_combined <<- addPolylines(
+        map = map_combined,
+        data = dfjams,
+        lng = ~line[[i]]$x,
+        lat = ~line[[i]]$y,
+        weight = 2,
+        opacity = 1,
+        color = "green"
+      )
+    }
+    
+  } else {
+    # extract alerts data
+    dfalerts <- as.data.frame(waze_data$alerts)
+    df <- dfalerts %>% filter(type %in% c(type_)) # get data depend on type
+    
+    # check alert type and set marker color
+    map_dfcolor <- df %>%
+      mutate(color = case_when(str_detect(type, "JAM") ~ "green",
+                               str_detect(type, "ROAD_CLOSED") ~ "blue",
+                               str_detect(type, "HAZARD") ~ "orange",
+                               str_detect(type, "ACCIDENT") ~ "red",
+                               TRUE ~ "a default")
+      )
+    
+    # create leaflet map
+    # add markers (alert); legend
+    map_combined <<- leaflet(
+      height = '100%'
+    ) %>%
+      addTiles() %>%
+      addLegend( # for circle markers
+        "topright",
+        title = "Map Marker Legend",
+        colors = c("green", "blue", "orange", "red"),
+        label = c("Jam", "Road Closed", "Hazard", "Accident"),
+        opacity = 1
+      ) %>%
+      addLegend( # for polylines
+        "bottomright",
+        title = "Map Polyine Legend",
+        colors = c("green"),
+        label = c("Jam"),
+        opacity = 1
+      ) %>%
+      addCircleMarkers(
+        data = map_dfcolor,
+        lng = ~location$x,
+        lat = ~location$y,
+        popup = ~subtype,
+        color = ~color,
+        opacity = 1,
+        fillOpacity = 1,
+        weight = 2,
+        radius = 4
+      )
   }
   
   # display map and legend
